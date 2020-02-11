fa5f0bd4a54ceee3792d5ee77a390c52c35cadf1
Otavio Cury <otaviocury.oc@gmail.com>
2020-02-11 17:06:46 -0300

    Removendo log4j

--- src/main/java/main/Extractor.java
+++ src/main/java/main/Extractor.java
@@ -5,7 +5,6 @@ import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.IOException;
-import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
